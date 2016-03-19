336b28ef3b848ac25116b8b45937403688790fe2
raa07 <teyn.k@mail.ru>
2016-03-19 23:39:51 +0200

    angular

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,15 @@
     <base href="/">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel='stylesheet' href='css/style.css'/>
+    <link rel='stylesheet' href="{{asset('css/style.css')}}"/>
 
+
+<script src="{{asset('scripts/app/lib/jquery-2.2.1.min.js')}}"></script>
+    <script src="{{asset('scripts/app/lib/angular-1.5.0-rc.0/angular.js')}}"></script>
+    <script src="{{asset('scripts/app/lib/angular-1.5.0-rc.0/angular-route.js')}}"></script>
+    <script src="{{asset('scripts/app/lib/angular-1.5.0-rc.0/angular-mocks.js')}}"></script>
+    <script src="{{asset('scripts/app/lib/ng-infinite-scroll.min.js')}}"></script>
+    <script src="{{asset('scripts/app/app.js')}}"></script>
 
 </head>
 <body>
