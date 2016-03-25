a8f9ec4bc8dda0205343138f53ffc24b9b03d388
raa07 <teyn.k@mail.ru>
2016-03-25 23:43:07 +0200

    appjs

--- public/scripts/app/app.js
+++ public/scripts/app/app.js
@@ -1,4 +1,4 @@
 var postsApp=angular.module('postsApp', ['infinite-scroll']);
-var authApp=angular.module('authApp', ['ui.router', 'satellizer']);
+var authApp=angular.module('authApp', ['satellizer']);
 
 var main=angular.module('mainApp',['authApp','postsApp','ngRoute']);
