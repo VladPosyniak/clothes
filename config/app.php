30b79966eb36179fe895b1b005ae63e3e012abd9
raa07 <teyn.k@mail.ru>
2016-05-01 14:13:16 +0300

    composer update

--- config/app.php
+++ config/app.php
@@ -164,9 +164,6 @@ return [
         App\Providers\RouteServiceProvider::class,
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
 
-
-         Laravel\Socialite\SocialiteServiceProvider::class,
-
     ],
 
     /*
@@ -214,8 +211,6 @@ return [
         'View'      => Illuminate\Support\Facades\View::class,
         'JWTAuth'   => Tymon\JWTAuth\Facades\JWTAuth::class,
         'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
-
-        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
 
     ],
 
